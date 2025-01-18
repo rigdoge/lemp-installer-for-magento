23adb6f2fbe96196ff932895dd6195ebc292f431
rigdoge <rigdoge@gmail.com>
2025-01-18 19:39:55 +0800

    fix: update next.js config to use standard output paths

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    // 使用更具体的文件名模式来避免冲突
-    config.output.assetModuleFilename = `static/media/[name].[hash][ext]`;
-    config.output.filename = `static/js/[name].[contenthash].js`;
-    config.output.chunkFilename = `static/js/[name].[contenthash].js`;
+  distDir: '.next',
+  poweredByHeader: false,
+  webpack: (config, { dev, isServer }) => {
+    // 只在生产环境修改输出配置
+    if (!dev) {
+      config.output = {
+        ...config.output,
+        // 使用标准的 Next.js 输出路径
+        filename: isServer ? '[name].js' : 'static/chunks/[name].[contenthash].js',
+        chunkFilename: isServer ? '[name].js' : 'static/chunks/[name].[contenthash].js'
+      };
+    }
     return config;
   }
 }
