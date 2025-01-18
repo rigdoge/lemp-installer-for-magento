bb7ca2ca97104e935c6ea424a7209e914c503d7f
rigdoge <rigdoge@gmail.com>
2025-01-18 19:37:28 +0800

    fix: update webpack config to avoid chunk conflicts

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    // 防止 webpack 创建不必要的目录
-    config.output.assetModuleFilename = `static/[hash][ext]`;
-    config.output.filename = `static/[hash].js`;
-    config.output.chunkFilename = `static/[hash].js`;
+    // 使用更具体的文件名模式来避免冲突
+    config.output.assetModuleFilename = `static/media/[name].[hash][ext]`;
+    config.output.filename = `static/js/[name].[contenthash].js`;
+    config.output.chunkFilename = `static/js/[name].[contenthash].js`;
     return config;
   }
 }
